79f3eed1cf7c3a0be30fa5c643de09ce5efe231f
David L. Jones <dlj@google.com>
2020-12-15 02:46:41 -0800

    Regenerate sources.

--- google/type/calendar_period_pb2.py
+++ google/type/calendar_period_pb2.py
@@ -12,7 +12,7 @@ from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
 
-importlib.import_module('_descriptors')
+importlib.import_module('google.api_common_protos._descriptors')
 sys.setdlopenflags(_dlopenflags)
 del _dlopenflags, ctypes, importlib, sys
 # @@protoc_insertion_point(imports)
